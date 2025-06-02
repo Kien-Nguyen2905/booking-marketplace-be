19b7ba1e14dcb74c3c2ad61f13da2f4c4d3ff174
Daisy <kiennguyen12.com@gmail.com>
2025-06-02 19:48:57 +0700

    fix custom zodserializer interceptor

--- src/shared/interceptor/transform.interceptor.ts
+++ src/shared/interceptor/transform.interceptor.ts
@@ -2,9 +2,10 @@ import { CallHandler, ExecutionContext, Injectable } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
 import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
-import { ZodSerializerDto } from 'nestjs-zod'
 import { ZodType } from 'zod'
 import { MessageKey } from 'src/shared/decorators/message.decorator'
+
+const ZOD_SERIALIZER_DTO_OPTIONS = 'ZOD_SERIALIZER_DTO_OPTIONS' as const
 
 @Injectable()
 export class CustomZodSerializerInterceptor {
@@ -19,7 +20,25 @@ export class CustomZodSerializerInterceptor {
     // Lấy message từ decorator @Message, mặc định là 'Success' nếu không có
     const defaultMessage = this.reflector.get<string | undefined>(MessageKey, context.getHandler()) ?? 'Successful'
     // Lấy schema từ decorator @ZodSerializerDto
-    const schema = this.reflector.get<ZodType<any> | undefined>(ZodSerializerDto, context.getHandler())
+    const dtoSchemaOrClass = this.reflector.getAllAndOverride<any>(ZOD_SERIALIZER_DTO_OPTIONS, [
+      context.getHandler(),
+      context.getClass(),
+    ])
+
+    let schema: ZodType<any> | undefined
+    if (dtoSchemaOrClass) {
+      if (typeof dtoSchemaOrClass === 'function') {
+        const maybeSchema = (dtoSchemaOrClass as any).schema
+        if (typeof maybeSchema === 'function') {
+          schema = maybeSchema()
+        } else {
+          schema = maybeSchema as ZodType<any>
+        }
+      } else if (typeof dtoSchemaOrClass === 'object' && 'parse' in dtoSchemaOrClass) {
+        // It is already a Zod schema instance
+        schema = dtoSchemaOrClass as ZodType<any>
+      }
+    }
 
     return next.handle().pipe(
       map((response) => {
